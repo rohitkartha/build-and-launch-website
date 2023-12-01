9950f9cc8ab5bc2792bc599091b6ab8325386cbb
rkartha <rkartha@umich.edu>
2023-11-30 20:14:57 -0500

    contact page responsiveness

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -6,45 +6,56 @@ import Header from "../components/Header.vue";
   <div class="container">
     <Header :logo="true" />
     <h1>Contact Us</h1>
-    <input
-      type="text"
-      name=""
-      id=""
-      placeholder="Name" />
 
-    <input
-      type="text"
-      name=""
-      id=""
-      placeholder="Email" />
-    <input
-      type="text"
-      name=""
-      id=""
-      placeholder="Message" />
-    <button>SEND</button>
+    <div class="messageFields">
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Name" />
+
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Email" />
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Message" />
+      <button>SEND</button>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .container {
   display: grid;
-  grid-template-rows: 1fr 2fr 1fr 1fr 1fr;
-  height: 60%;
+  grid-template-rows: 1fr 1fr 3fr 1fr;
+  height: 70%;
 }
 
 h1 {
   text-align: left;
   color: black;
   font-weight: bold;
+  margin-top: 0;
+}
+
+.messageFields {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 50%;
 }
 
 input {
-  width: 450px;
-  height: 40px;
   border: 4px solid black;
   border-radius: 20px;
   padding-left: 20px;
+  width: calc(100% - 30px);
+  margin: 12px 12px 0px 0px;
 }
 
 input::placeholder {
@@ -55,6 +66,13 @@ input::placeholder {
 
 button {
   background: black;
-  width: 480px;
+  width: 100%;
+  margin: 12px 12px 0px 0px;
+}
+
+@media (height < 500px) {
+  .container {
+    height: 100%;
+  }
 }
 </style>
